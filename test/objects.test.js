c321b9e379f39e12d94801e548411fc20f191442
just-paja <pavel@proboston.net>
2014-09-10 11:28:54 +0200

    Test container object

---
+++ test/objects.test.js
@@ -0,0 +1,25 @@
+var
+	assert = require('assert'),
+	extend = require('xtend');
+
+require('../lib/pwf');
+require('../lib/models/container');
+require('../lib/models/domel');
+require('../lib/models/caller');
+
+describe('object module', function() {
+	it('tests container object', function() {
+		var obj = pwf.create('container', {
+			'jebka':'foo',
+			'bar':{
+				'food':'burger'
+			}
+		});
+
+		assert.equal(obj.get('jebka'), 'foo');
+		assert.equal(obj.get('bar.food'), 'burger');
+
+		obj.set('bar.food', 'pizza');
+		assert.equal(obj.get('bar.food'), 'pizza');
+	});
+});
