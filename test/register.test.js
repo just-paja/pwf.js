e5864a9d97ab2c65c2cd749a76847465e849a64b
just-paja <pavel@proboston.net>
2014-11-14 16:15:49 +0100

    Module states
    * Method get_module_status returns int describing module current status

--- test/register.test.js
+++ test/register.test.js
@@ -20,13 +20,13 @@ describe('module registering', function() {
 		pwf.wait_for('module', ['modd'], function() { modx_run = true; });
 
 		assert.strictEqual(typeof pwf.modd, 'object', 'Should be object, otherwise the module modd is not registered.');
-		assert.strictEqual(pwf.get_module_status('modd'), false, 'Should be false, because modd does not have dependencies ready.');
+		assert.strictEqual(pwf.status('modd'), false, 'Should be false, because modd does not have dependencies ready.');
 
 		pwf.reg_module('mod1', function() {});
 
 		assert.strictEqual(typeof pwf.mod1, 'object', 'Should be object, otherwise the module mod1 is not registered properly.');
-		assert.strictEqual(pwf.get_module_status('mod1'), true, 'Should be true, mod1 needs no dependencies, so it should be initialized in place.');
-		assert.strictEqual(pwf.get_module_status('modd'), true, 'Should be true, because mod1 should have fired init_remaining.');
+		assert.strictEqual(pwf.status('mod1'), true, 'Should be true, mod1 needs no dependencies, so it should be initialized in place.');
+		assert.strictEqual(pwf.status('modd'), true, 'Should be true, because mod1 should have fired init_remaining.');
 		assert.strictEqual(modx_run, true, 'Should be true, because modd should have fired callbacks.');
 	});
 });
