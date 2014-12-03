815369933b28873911f48cc28175d142bd1618a3
just-paja <just.paja@gmail.com>
2014-12-03 12:41:52 +0100

    Method get_status
    Returns current status of module or class by name

--- test/register.test.js
+++ test/register.test.js
@@ -20,13 +20,13 @@ describe('module registering', function() {
 		pwf.wait_for('module', ['modd'], function() { modx_run = true; });
 
 		assert.strictEqual(typeof pwf.modd, 'object', 'Should be object, otherwise the module modd is not registered.');
-		assert.strictEqual(pwf.status('modd'), false, 'Should be false, because modd does not have dependencies ready.');
+		assert.strictEqual(pwf.get_status('module', 'modd'), 1, 'Should be 1, because modd does not have dependencies ready.');
 
 		pwf.reg_module('mod1', function() {});
 
 		assert.strictEqual(typeof pwf.mod1, 'object', 'Should be object, otherwise the module mod1 is not registered properly.');
-		assert.strictEqual(pwf.status('mod1'), true, 'Should be true, mod1 needs no dependencies, so it should be initialized in place.');
-		assert.strictEqual(pwf.status('modd'), true, 'Should be true, because mod1 should have fired init_remaining.');
+		assert.strictEqual(pwf.get_status('module', 'mod1'), 3, 'Should be 3, mod1 needs no dependencies, so it should be initialized in place.');
+		assert.strictEqual(pwf.get_status('module', 'modd'), 3, 'Should be 3, because mod1 should have fired init_remaining.');
 		assert.strictEqual(modx_run, true, 'Should be true, because modd should have fired callbacks.');
 	});
 });
