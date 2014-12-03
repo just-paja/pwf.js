a6126a1f8a4b120c68f12d90dacbd79b9af02f43
just-paja <just.paja@gmail.com>
2014-12-03 12:45:03 +0100

    Tests for get_status

--- test/class.init.test.js
+++ test/class.init.test.js
@@ -19,10 +19,19 @@ describe('class init', function() {
 				}
 			};
 
+		assert.equal(pwf.get_status('class', 'test1'), 0);
 		pwf.reg_class({'name':'test1', 'init':fn1, 'storage':{'pass':[]}});
-		pwf.reg_class({'name':'test2', 'init':fn2, 'parents':['test1']});
+		assert.equal(pwf.get_status('class', 'test1'), 3);
+
 		pwf.reg_class({'name':'test3', 'init':fn3, 'parents':['test2']});
+		assert.equal(pwf.get_status('class', 'test3'), 1);
+
+		pwf.reg_class({'name':'test2', 'init':fn2, 'parents':['test1']});
+		assert.equal(pwf.get_status('class', 'test2'), 3);
+		assert.equal(pwf.get_status('class', 'test3'), 3);
+
 		pwf.reg_class({'name':'test4', 'init':fn4, 'parents':['test3'], 'public':helper});
+		assert.equal(pwf.get_status('class', 'test4'), 3);
 
 		obj = pwf.create('test4');
 		assert.equal(obj.get_pass().join(''), '1234');
