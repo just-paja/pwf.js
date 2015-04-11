dd2552fb209a20ad8ac2262bc95db64f72725499
just-paja <just.paja@gmail.com>
2015-04-11 18:06:23 +0200

    Code complexity reduce

--- test/class.internal.test.js
+++ test/class.internal.test.js
@@ -31,35 +31,6 @@ describe('class internals', function() {
 	});
 
 
-	it('class instance internal methods', function() {
-		var obj;
-
-		pwf.reg_class('test.internal', {
-			'public':{
-				'type':function(proto, name) {
-					return proto.type(name);
-				},
-
-				'exists':function(proto, name) {
-					return proto.exists(name);
-				},
-			},
-
-			'proto':{
-				'test':function(){}
-			}
-		});
-
-		obj = pwf.create('test.internal');
-
-		assert.equal(obj.type('test'), 'function');
-		assert.equal(obj.type('test2'), 'undefined');
-
-		assert.equal(obj.exists('test'), true);
-		assert.equal(obj.exists('test2'), false);
-	});
-
-
 	it('class instance parents', function() {
 		var obj;
 
